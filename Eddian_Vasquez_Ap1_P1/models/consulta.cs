c8b1c273129d1187757ec7ed0a5df99b698b8bd6
eddianvasquez <eddian_vasquez@ucne.edu.do>
2025-06-09 18:22:01 -0400

    listo para iniciar

---
+++ Eddian_Vasquez_Ap1_P1/models/consulta.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eddian_Vasquez_Ap1_P1.models
+{
+    public class consulta
+    {
+        [Key]
+        public int id { get; set; }
+    }
+}
