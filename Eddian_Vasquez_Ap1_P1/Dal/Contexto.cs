023d853bbb4080c06aadc1150d192db34d737e04
eddianvasquez <eddian_vasquez@ucne.edu.do>
2025-06-09 18:38:51 -0400

    agregando contexto

--- Eddian_Vasquez_Ap1_P1/Dal/Contexto.cs
+++ Eddian_Vasquez_Ap1_P1/Dal/Contexto.cs
@@ -1,6 +1,16 @@
-﻿namespace Eddian_Vasquez_Ap1_P1.Dal
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Eddian_Vasquez_Ap1_P1.Dal
 {
-    public class Contexto
+    public class Contexto DbContext
     {
+        public Dbset<aporte> Aportes { get; set; }
+
+    public contexto(DbContextOptions<Contexto> options :base(options){ }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<aporte>()Haskey(app => a.AporteId)
+    }
     }
 }
