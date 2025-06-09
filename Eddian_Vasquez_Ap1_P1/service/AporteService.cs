12d87fd02ba2510be4820632c24fd44dd1b0b9c7
eddianvasquez <eddian_vasquez@ucne.edu.do>
2025-06-09 19:04:41 -0400

    agregando Aporte service

---
+++ Eddian_Vasquez_Ap1_P1/service/AporteService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repaso.models;
+using DAL;
+using Eddian_Vasquez_Ap1_P1.Dal;
+
+
+namespace repaso.Services
+{
+    public class AportesService
+    {
+        private readonly Contexto _contexto;
+
+        public AportesService(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+        public async Task<List<Aportes>> obtenerAportesasync()
+        {
+            return await _contexto.Aportes.findAsync(int id)
+                {
+                public async Task CrearAporteAsync(Aporte aporte)
+        {
+            Microsoft.EntityFrameworkCore.ChangeTracking.EntityEntry<Aporte> entityEntry = _contexto.Aportes.Add(aporte);
+            await _contexto.SaveChangesAsync();
+        }
+        
+        public async Task EditarAportesasync(Aporte aporte)
+        {
+            _contexto.entry(aporte).state = EntityState.Modified;
+            await _contexto.SaveChangesAsync();
+
+        }
+        public async Task EliminarAportesasync(int id)
+        {
+            var aportes = await _contexto.Aportes.findAsync(id);
+            if (aportes != null)
+            {
+                _contexto.Aportes.Remove(aporte);
+                await _contexto.SaveChangesAsync();
+            }
+        }
+        }
+    }
+        
