0fa7748d1fa1edee2e31f2542e177136079e35e5
eddianvasquez <eddian_vasquez@ucne.edu.do>
2025-06-09 18:49:48 -0400

    agregando aportes

---
+++ Eddian_Vasquez_Ap1_P1/models/Aporte.cs
@@ -0,0 +1,11 @@
+﻿namespace Repaso.models
+{
+    public class Aporte
+    {
+        public int AporteId { get; set; }
+        public string Persona { get; set; }
+        public decimal Monto { get; set; }
+        public string Observacion { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+    }
+}
